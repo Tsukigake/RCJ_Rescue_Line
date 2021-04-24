7d08ad78a1aab1f15e870b512c9e917124b51016
kamiokannde <kamiokannde829@gmail.com>
2021-04-24 21:21:42 +0900

    Create setup_nano.h

---
+++ nano/setup_nano.h
@@ -0,0 +1,21 @@
+//MDM control system
+#define no_motion 0
+#define stop 1
+#define brake 2
+
+//LED
+#define on 1
+#define off 0
+
+//button
+#define GREEN 0
+#define RED 1
+
+//I2C
+#define first_motor_x 1
+#define first_motor_y 2
+#define first_motor 3
+#define second_motor_x 4
+#define second_motor_y 5
+#define second_motor 6
+#define third_motor 8

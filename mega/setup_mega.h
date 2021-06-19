727afc6a3b628b188ec27f5b8c596fbd211a0272
kamiokannde <kamiokannde829@gmail.com>
2021-06-19 22:55:34 +0900

    Update setup_mega.h

--- mega/setup_mega.h
+++ mega/setup_mega.h
@@ -1,21 +1,68 @@
 //MDM control system
-#define no_motion 0
-#define stop 1
-#define brake 2
+#define no_motion   0
+#define stop        1
+#define brake       2
 
 //LED
-#define ON 1
-#define OFF 0
+#define ON      1
+#define OFF     0
 
 //button
-#define GREEN 0
-#define RED 1
+#define GREEN   0
+#define RED     1
 
 //I2C
-#define first_motor_x 1
-#define first_motor_y 2
-#define first_motor 3
-#define second_motor_x 4
-#define second_motor_y 5
-#define second_motor 6
-#define third_motor 8
+#define first_motor_x   1
+#define first_motor_y   2
+#define first_motor     3
+#define second_motor_x  4
+#define second_motor_y  5
+#define second_motor    6
+#define third_motor     8
+
+//VL53L0X
+#define DEFAULT                 0
+#define LONG_RANGE              1
+#define HIGH_SPEED              2
+#define HIGH_ACCURACY           3
+#define distance_mode           0
+#define coordinate_setup_mode   1
+#define coordinate_mode         2
+
+
+//line sensor
+//  A15 GIA01   D49 GID03   A11 GIA05   D39 GID08   A06 GIA10
+//
+//  D53 GID01   D47 GID04   A10 GIA06   D37 GID09   D31 GID12
+//
+//  A14 GIA02   A12 GIA04   D41 GID07   A07 GIA09   A05 GIA11
+//
+//  D51 GID02   D45 GID05   A09 GIA07   D35 GID10   D29 GID13
+//
+//  A13 GIA03   D43 GID06   A08 GIA08   D33 GID11   A04 GIA12
+
+//color sensor
+//  A15 GIC01   A06 GIC02
+//
+//  A13 GIC04   A04 GIC03
+
+//VL53L0X
+//      1                   5   6
+//
+//  4       2           7           8
+//
+//      3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
